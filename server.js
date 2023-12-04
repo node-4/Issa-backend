579c121f6ab17443cec336bb3b02e9dabede8b82
rubiyadav <rubiflyeis@gmail.com>
2023-12-04 11:13:35 +0530

    update

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const compression = require("compression");
+const serverless = require("serverless-http");
+const path = require("path");
+const app = express();
+app.use(compression({ threshold: 500 }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.get("/", (req, res) => {
+        res.send("Hello World issa-Backend!");
+});
+
+
+
+
+
+mongoose.Promise = global.Promise;
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGO_URI,).then((data) => {
+        console.log(`Mongodb connected with server: ${data.connection.host} : Issa-Backend`);
+});
+app.listen(process.env.PORT, () => {
+        console.log(`Listening on port ${process.env.PORT}!`);
+});
+module.exports = { handler: serverless(app) };
