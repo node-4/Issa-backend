161e747123723368e187900d2b56c5ee02e75b26
rubiyadav <rubiflyeis@gmail.com>
2024-02-15 12:20:35 +0530

    update

--- model/Notes/notes.js
+++ model/Notes/notes.js
@@ -266,160 +266,16 @@ const addressSchema = new mongoose.Schema({
         location: {
                 type: String,
         },
-        alaram1: [{
-                date: {
-                        type: Date,
-                },
+        alaramDate: {
+                type: Date,
+        },
+        alaram: [{
                 working: {
                         type: String,
                         enum: ["working", "NeedRepair", "RepairAndWork"]
                 }
         }],
-        alaram2: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram3: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram4: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram5: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram6: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram7: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram8: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram9: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram10: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram11: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram12: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram13: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram14: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        alaram15: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        extinguisher1: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        extinguisher2: [{
-                date: {
-                        type: Date,
-                },
-                working: {
-                        type: String,
-                        enum: ["working", "NeedRepair", "RepairAndWork"]
-                }
-        }],
-        extinguisher3: [{
+        extinguisher: [{
                 date: {
                         type: Date,
                 },
@@ -429,19 +285,16 @@ const addressSchema = new mongoose.Schema({
                 }
         }],
         staff: [{
+                name: {
+                        type: String,
+                },
                 signature: {
+                        type: String,
+                },
+                initial: {
                         type: String,
                 }
         }],
-        fireExtinguisherExpire1: {
-                type: Date,
-        },
-        fireExtinguisherExpire2: {
-                type: Date,
-        },
-        fireExtinguisherExpire3: {
-                type: Date,
-        },
         adminId: {
                 type: schema.Types.ObjectId,
                 ref: "user",
